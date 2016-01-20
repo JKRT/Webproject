a70e833d67c1cc2e874ab60e9e699b42e479c916
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-01-20 10:07:06 +0100

    Collab blablalba javascript.

--- RF1_TDDD97/client.js
+++ RF1_TDDD97/client.js
@@ -1,7 +1,17 @@
  displayView = function() {
-    document.getElementById("content").innerHTML = document.getElementById("welcomeView").text;
+   document.getElementById("content").innerHTML = document.getElementById("welcomeView").text;
 };
 
+
+validPasswd = function (password, required_password) {
+ alert("HELLO BABY");
+}
+
 window.onload = function() {
-    displayView();
+
+    var password = document.getElementById("password");
+    var repeatPsw = document.getElementById("repeatPsw");
+
+    password.onchange = validPasswd(document.getElementById("password"),document.getElementById("repeatPsw"));
+    repeatPsw.onkeyup = (validPasswd (document.getElementById("password"),document.getElementById("repeatPsw")));
 };
