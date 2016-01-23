08edf628e5cc77be0f585e1aa9d65eeec8dc6ac5
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-01-23 11:29:21 +0100

    Collab. commit, rounded corners baby. Yea.

--- client/client.js
+++ client/client.js
@@ -1,28 +1,25 @@
 displayView = function() {
     console.log("Display view called");
     if(sessionStorage.token)  {
-	document.getElementById("content").innerHTML = document.getElementById("profileView").text;
-    } else { 
-	console.log("trying to fetch welcome view");
-	document.getElementById("content").innerHTML = document.getElementById("welcomeView").text;
+        document.getElementById("content").innerHTML = document.getElementById("profileView").text;
+        showHome();
+    } else {
+        console.log("trying to fetch welcome view");
+        document.getElementById("content").innerHTML = document.getElementById("welcomeView").text;
     }
 };
 
 
-checkSignUp = function() {
-    var password = document.getElementById("password2").value;
-    var enteredPassword  = document.getElementById("repeatPsw").value ;
+function checkSignUp() {
+    if(document.getElementById("password2").value === document.getElementById("repeatPsw").value) {
+        var dataObject = {email: document.getElementById("email2").value,
+                        password: document.getElementById("password2").value,
+                        firstname: document.getElementById("firstName").value,
+                        familyname: document.getElementById("familyName").value,
+                        gender: document.getElementById("gender").value,
+                        city: document.getElementById("city").value,
+                        country: document.getElementById("country").value};
 
-
-    var dataObject = {email: document.getElementById("email2").value,
-                      password: document.getElementById("password2").value,
-                      firstname: document.getElementById("firstName").value,
-                      familyname: document.getElementById("familyName").value,
-                      gender: document.getElementById("gender").value,
-                      city: document.getElementById("city").value,
-                      country: document.getElementById("country").value};
-
-    if( password === enteredPassword ) {
         var signUpObject = serverstub.signUp(dataObject);
         if (!signUpObject.success) {
             alert(signUpObject.message);
@@ -35,7 +32,7 @@ checkSignUp = function() {
     return false;
 }
 
-checkLogin = function() {
+function checkLogin() {
     var email = document.getElementById("email1").value;
     var password = document.getElementById("password1").value;
     var signInObject = serverstub.signIn(email,password);
@@ -44,14 +41,14 @@ checkLogin = function() {
         alert(signInObject.message);
         return false;
     } else {
-	//Save the token in the browser...
-	sessionStorage.token = signInObject.data;
-	return true;
+        //Save the token in the browser...
+        sessionStorage.token = signInObject.data;
+        return true;
     }
 
 }
 
-showHome = function() {
+function showHome() {
     document.getElementById("homePanel").style.display = "block";
     document.getElementById("chatPanel").style.display = "block";
     document.getElementById("lowerLogoSloganPanel").style.display = "block";
@@ -65,57 +62,50 @@ showHome = function() {
     document.getElementById("homeCountry").innerHTML = homePanelObject.data.country;
     var chatLog = "";
     for (var message of serverstub.getUserMessagesByToken(sessionStorage.token).data) {
-	chatLog += "<strong>" + message.writer + "</strong>: " + message.content + "<br>";
+        chatLog += "<strong>" + message.writer + "</strong>: " + message.content + "<br>";
     } 
 
     document.getElementById("chatPanel").innerHTML = chatLog;
 }
 
-showAccount = function() {
+function showAccount() {
     document.getElementById("accountPanel").style.display = "block";
     document.getElementById("homePanel").style.display = "none";
     document.getElementById("chatPanel").style.display = "none";
     document.getElementById("lowerLogoSloganPanel").style.display = "none";
 }
 
-logout = function() {
+function logout() {
     console.log("logoutFunctionCalled");
     alert(serverstub.signOut(sessionStorage.token).message);
     sessionStorage.removeItem("token");
     displayView();
 }
 
-reloadMessages = function() {
+function reloadMessages () {
     showHome();
 }
 
-postMessage = function(recipient) {
+function postMessage(recipient) {
     var messageContent = document.getElementById("chatBox");
     alert(serverstub.postMessage(sessionStorage.token, messageContent.value, recipient).message);
     messageContent.value = "";
     reloadMessages();
 }
 
-postMessageToSelf = function() {
+function postMessageToSelf() {
     var homePanelObject = serverstub.getUserDataByToken(sessionStorage.token);
     postMessage(homePanelObject.data.email);
 }
 
 
-changePassword = function() {
+function changePassword() {
     console.log("Called changePassword()");
     alert(serverstub.changePassword(sessionStorage.token, 
-				    document.getElementById("oldPassword").value,
-				    document.getElementById("newPassword").value).message);
+                document.getElementById("oldPassword").value,
+                document.getElementById("newPassword").value).message);
 }
 
 window.onload = function() {
     displayView();
-    if (!sessionStorage.token) {
-	document.getElementById("signUpForm").onsubmit = function() {return checkSignUp()}; 
-	document.getElementById("loginForm").onsubmit = function() {return checkLogin()}; 
-    } else {
-	showHome();
-	document.getElementById("changePasswordForm").onsubmit = function() {return changePassword()}; 
-    }
 };
