af1fdbae55a41740f5dc6f8f03df8a6cb358648d
John Tinnerholm <johti139@student.liu.se>
2016-02-02 17:04:21 +0100

    Collab commit implemented  change_password and so forth except post_message, post_message needs to be fixed

--- server/database_helper.py
+++ server/database_helper.py
@@ -25,6 +25,7 @@ def sign_out(token):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM users WHERE token=?", (token,))
+  
     if cursor.fetchone() != None:
         cursor.execute("UPDATE users SET token=NULL WHERE token=?", (token,))
         connection.commit()
@@ -35,6 +36,111 @@ def sign_out(token):
         connection.close()
         return  json.dumps({"success": False,
                             "message": "You are not signed in."})
+
+
+def change_password(token,old_password,new_password):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE token=?", (token,))
+
+    if cursor.fetchone() == None:
+        return json.dumps({"success": False, "message": "You are not logged in."})
+        
+    else:
+        cursor.execute("SELECT password FROM users WHERE token=? AND password=?", (token,old_password))
+        if cursor.fetchone() != None:
+            cursor.execute("UPDATE users SET password=? WHERE token=?", (new_password,token))
+            connection.commit()
+            connection.close()
+            return json.dumps( {"success": True, "message": "Password changed."} )
+        else:
+            connection.close()
+            return json.dumps({"success": False, "message": "Wrong password."})
+
+
+def get_user_data_by_token(token):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT first_name,family_name,email,city,country,gender FROM users WHERE token=?", (token,))
+    cursorObject = cursor.fetchone()
+    
+    if cursorObject  == None:
+        connection.close()
+        return json.dumps( {"success": False, "message": "You are not signed in."})
+    else:
+        match = {"firstname": cursorObject[0] , "familyname":cursorObject[1] , "email": cursorObject[2] 
+                 , "city": cursorObject[3] , "country": cursorObject[4] , "gender": cursorObject[5]}   
+        connection.close()
+        return json.dumps({"success": True, "message": "User data retrieved.", "data": match})
+    
+def get_user_data_by_email(token,email):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT first_name FROM users WHERE token=?",(token,))
+    
+    if cursor.fetchone() == None:
+        connection.close()
+        return json.dumps( {"success": False, "message": "You are not signed in."})
+    
+    cursor.execute("SELECT first_name,family_name,email,city,country,gender FROM users WHERE email=?",(email,))
+    cursorObject = cursor.fetchone()
+
+    if cursorObject  == None:
+        connection.close()
+        return json.dumps( {"success": False, "message": "No such user."})
+    else:
+        match = {"firstname": cursorObject[0] , "familyname":cursorObject[1] , "email": cursorObject[2] 
+                 , "city": cursorObject[3] , "country": cursorObject[4] , "gender": cursorObject[5]}   
+        connection.close()
+        return json.dumps({"success": True, "message": "User data retrieved.", "data": match})
+
+
+def get_user_messages_by_token(token):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE token=?", (token,))
+    if cursor.fetchone() == None:
+        connection.close()
+        return json.dumps({"success": False, "message": "You are not logged in."})
+
+    messages = []
+    for row in cursor.execute("SELECT contents FROM messages WHERE recipient_id=(SELECT id FROM users where token =? ) ORDER BY sent_date ASC" , (token, ) ):
+        messages.append(row[0])
+    connection.close()
+    return json.dumps({"success": True, "message": "User data retrieved.", "data": messages})
+
+
+def get_user_messages_by_email(token,email):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT first_name FROM users WHERE token=?",(token,))
+    
+    if cursor.fetchone() == None:
+        connection.close()
+        return json.dumps( {"success": False, "message": "You are not signed in."})
+        
+    cursor.execute("SELECT first_name FROM users WHERE email=?",(email,))
+    
+    if cursor.fetchone() == None:
+        connection.close()
+        return json.dumps( {"success": False, "message": "No such user."})
+
+    messages = []
+    for row in cursor.execute("SELECT contents FROM messages WHERE recipient_id=(SELECT id FROM users WHERE email=? ) ORDER BY sent_date ASC" , (email, ) ):
+        messages.append(row[0])
+    connection.close()
+    return json.dumps({"success": True, "message": "User data retrieved.", "data": messages})
+
+
+
+def post_message(token, message mail):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT first_name FROM users WHERE token =? " , (token,))
+
+    if cursor.fetchone() == None:
+        return json.dumps( {"success": False, "message": "You are not signed in."})
+
 
 
 def generate_token():
--- server/server.py
+++ server/server.py
@@ -22,5 +22,60 @@ def sign_out(token = None):
     return database_helper.sign_out(token)
 
 
+@app.route('/change_password' , methods=['POST'])
+def change_password(token = None, old_password=None ,new_password=None):
+    old_password = request.form['oldPassword']
+    new_password = request.form['newPassword']
+    token = request.form['token']
+    
+    #Initial handling before talking to the database.
+    if old_password == None or new_password == None or token == None:
+        return json.dumps({"success": False, "message": "You are not logged in."})
+    else:
+        return database_helper.change_password(token,old_password, new_password)
+
+@app.route('/get_user_data_by_token' , methods=['POST'])
+def get_user_data_by_token(token = None):
+    token = request.form['token']
+    if token == None:
+        return json.dumps({"success": False, "message": "You are not signed in."})
+    return database_helper.get_user_data_by_token(token)
+
+@app.route('/get_user_data_by_email', methods=['POST'])
+def get_user_data_by_email(token = None , email = None):
+    token = request.form['token']
+    email = request.form['email']
+
+    if token == None:
+        return json.dumps({"success": False, "message": "You are not signed in."})
+    return database_helper.get_user_data_by_email(token, email)
+
+
+@app.route('/get_user_messages_by_token' , methods=['POST'])
+def get_user_messages_by_token(token = None): 
+    token = request.form['token']
+
+    if token == None:
+        return json.dumps({"success": False, "message": "You are not signed in."})
+    return database_helper.get_user_messages_by_token(token)
+
+@app.route('/get_user_messages_by_email' , methods=['POST'])
+def get_user_messages_by_email(token = None, email = None): 
+    token = request.form['token']
+    email = request.form['email']
+
+    if token == None or email == None:
+        return json.dumps({"success": False, "message": "You are not signed in."})
+    return database_helper.get_user_messages_by_email(token, email)
+
+@app.route('/post_message',methods = ['POST'])
+def post_message(token = None,message = None ,email = None): 
+    token = request.form['token']
+    message = request.form['message']
+    email = request.form['email']
+
+    if token == None or message == None or email == None:  
+        return json.dumps({"success": False, "message": "You are not signed in."})
+
 if __name__ == '__main__':
     app.run(debug=True)
