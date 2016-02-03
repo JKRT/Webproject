02efa643ee2ab9305f838fc33c7d8d42e687f4cf
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-02-03 09:56:18 +0100

    Collab commit, generator script updated.

--- server/test/generateSQL.py
+++ server/test/generateSQL.py
@@ -19,6 +19,7 @@ for data in inputList:
     print addQuotes(data_list[3]) + ","
     print addQuotes(data_list[4]) + ","
     print addQuotes(data_list[5]) + ","
-    print addQuotes(data_list[6]) + " );"
+    print addQuotes(data_list[6]) + "," 
+    print "NULL );"
     print "\n" 
 
