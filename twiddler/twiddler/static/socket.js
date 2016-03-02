e3c4adcb43dd23eea9b3ee6bb8add8c1106960bd
John Tinnerholm <johti139@student.liu.se>
2016-03-02 23:45:40 +0100

    Charts are now loaded upon load and refresh

--- twiddler/twiddler/static/socket.js
+++ twiddler/twiddler/static/socket.js
@@ -4,17 +4,23 @@ TwiddlerSocket = function(email, token) {
     var userEmail = email;
     var userToken = token;
     var handle = new WebSocket("ws://localhost:7777/websocket");
+    var thisClass = this;
     console.log("Connecting to Twiddler WebSocket...");
     console.log("State of connection is '" + handle.readyState + "'.");
 
     handle.onopen = function(event) {
         console.log("Twiddler WebSocket has opened!");
         console.log("Sending token and email, hoping to gain access...");
-	if(userEmail != null)
+	if(userEmail != null) {
+	    /*Normal mode request the opening of a new socket */
             data = {email: userEmail, token: userToken};
-	else 
+	} else {
+	    /*When a refresh is suggested */
 	    data = {token:userToken};
-        handle.send(JSON.stringify(data));
+            handle.send(JSON.stringify(data));
+	}
+	console.log("Fetch livedata");
+	thisClass.initData(userToken);
     };
 
     this.close = function() { 
